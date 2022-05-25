e6fcdaa4d86c241ba3424561ef69e9a4ef76755b
ChrisJ <EpicLegitPlayer@gmail.com>
2022-05-25 16:57:00 +0800

    db refactoring

--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,53 @@
 <?php
 //adjust names
-	$host = 'localhost';
-	$user = 'root';
-	$pass = '';
-	$db = 'fumo_db';
-	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+	class DBobject {
+		private $host;
+		private $user;
+		private $pass;
+		private $db;
+		private $mysqli;
+
+		public function __construct() {
+			$this->host = 'localhost';
+			$this->user = 'pma';
+			$this->pass = '';
+			$this->db = 'fumo_db';
+			$this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db) or die($this->mysqli->error);
+
+			if($this->mysqli->connect_error){
+				die("Connection failed".$this->mysqli->connect_error);
+			}
+			echo "Connected successfully";
+		}
+
+		public function insert_user_data($user_email,$user_pass){
+
+			$sql = "INSERT INTO `user_info` (`User_email`,`User_password`) VALUES ('$user_email','$user_pass')";
+			mysqli_query($this->mysqli, $sql);
+
+			echo "User Data Inserted";
+		}
+
+		function update_device_info($id_find,$user_email){
+
+			$update_user = "UPDATE available_devices SET owner_email = '$user_email' WHERE device_id = '$id_find';";
+			mysqli_query($this->mysqli, $update_user);
+
+			echo "Device data updated";
+		  }
+	  
+		function device_exists($id_find){
+
+			$find_device = "SELECT * FROM `available_devices` WHERE `device_id` LIKE $id_find;";
+			$sql=mysqli_query($this->mysqli, $find_device);
+
+			if($sql->num_rows == 0){
+				return FALSE;
+			}
+			return TRUE;
+		
+		}
+
+	}
 	
 ?>
--- send_user_data.php
+++ send_user_data.php
@@ -1,61 +1,21 @@
 <?php
-    include_once 'db_conn.php';
+    include 'db_conn.php';
 
     $user_email=$_POST['user_email'];
     $user_pass=$_POST['user_password'];
     $id_find = $_POST['device_id'];
 
-    if($mysqli->connect_error){
-        die("Connection failed".$mysqli->connect_error);
-    }
-      echo "Connected successfully";
-
     if ($id_find != null && $id_find != ''){
-      if(checkDevice($id_find)==TRUE){
-        insert_user_data($user_email,$user_pass);
+      $db_conn = new DBobject;
 
-
-        update_device_info($id_find,$user_email);
-        
-      }
-    }else{
-      insert_user_data($user_email,$user_pass);
-    }
-
-    
-
-    function insert_user_data($user_email,$user_pass){
-      include 'db_conn.php';
-
-      $sql = "INSERT INTO `user_info` (`User_email`,`User_password`) VALUES ('$user_email','$user_pass')";
-
-      $send_data =  mysqli_query($mysqli,$sql);
-
-      echo "sent";
-    }
-    
-    function update_device_info($id_find,$user_email){
-      include 'db_conn.php';
-
-      $check_user = "SELECT *  FROM `user_info` WHERE `User_email` LIKE $user_email;";
-
-      $check = mysqli_query($mysqli,$check_user);
-
-      $insert_user = "UPDATE `available_devices` SET `owner_email` = $user_email WHERE `available_devices`.`device_id` = $id_find;";
-
-      $send_data =  mysqli_query($mysqli,$insert_user);
-    }
-
-    function checkDevice($id_find){
-      include 'db_conn.php';
+      $db_conn->insert_user_data($user_email,$user_pass);
       
-      $find_device = "SELECT *  FROM `available_devices` WHERE `device_id` LIKE $id_find;";
-      $sql=mysqli_query($mysqli,$find_device);
-      if($sql->num_rows > 0){
-        return TRUE;
-      }else{
-        return FALSE;
+      if($db_conn->device_exists($id_find)==TRUE){
+        $db_conn->update_device_info($id_find,$user_email);
       }
 
     }
+
+    header("Location: client_view.html");
+    die();
 ?>
