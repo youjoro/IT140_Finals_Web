c873d5d44e763de64a641d70681f9ce399298a6a
ChrisJ <EpicLegitPlayer@gmail.com>
2022-05-28 12:46:54 +0800

    added log_data

--- restAPI/log_data.php
+++ restAPI/log_data.php
@@ -1,12 +1,21 @@
 <?php
+    error_reporting(E_ALL ^ E_WARNING); 
     // Change Params if needed
-    // Three known params so far: temperature, moisture, humidity
+    // Four known params so far: temperature, moisture, humidity
 
     $temp = $_GET['temp'];
     $moisture = $_GET['moisture'];
     $humidity = $_GET['humidity'];
+    $date = $_GET['date'];
 
-    $txt = "user id date";
+    $data = [
+        'Temperature' => $temp,
+        'Moisture' => $moisture,
+        'Humidity' => $humidity,
+        'Date' => $date,
+    ];
+
+    $txt = json_encode($data);
     $myfile = file_put_contents('arduino.log', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 
     die();
