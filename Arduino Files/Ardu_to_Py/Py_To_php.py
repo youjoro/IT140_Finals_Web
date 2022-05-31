d189991a798bb1ce4c50de1c8fa712882fd06f63
youjoro <78091990+youjoro@users.noreply.github.com>
2022-05-31 22:06:08 +0800

    Python to PHP (incomplete) still not sending data with params

--- Arduino Files/Ardu_to_Py/Py_To_php.py
+++ Arduino Files/Ardu_to_Py/Py_To_php.py
@@ -20,24 +20,23 @@ def read_userOUTPUT():
     return mensahe
 
 #retrieving data from the serial port and reformats to be able to be sent to the php
-def send_data():
-    data_received = read_userOUTPUT()
-    data_received = data_received.split()
-    print(data)
+def send_data(data_get):
+    data_received = data_get.split()
+    print(data_get)
     data["temp"] = data_received[0]
     data["moisture"] = data_received[1]
     data["Humidity"] = data_received[2]
     data["date"] = datetime.datetime.now()
 
-    r = requests.get("http://192.168.100.18C/IT140_Finals_Web/restAPI/log_data.php", params=data)
-    data.clear()
+    r = requests.get('http://localhost//IT140_Finals_Web/restAPI/log_data.php', params=data)
     return
 
 #loops the python scrips so that unless the read output from the serial port is empty
 #the program will not send the data to the php file
 data_received = read_userOUTPUT()
-while data_received == '':
+while True:
     data_received = read_userOUTPUT()
+
     if (data_received != ''):
         send_data(data_received)
 
